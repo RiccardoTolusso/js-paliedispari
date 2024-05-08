a1b384c1d1e161dd7cf75acccbea93c009486cb0
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-08 10:24:42 +0200

    first commit

---
+++ palindroma/js/main.js
@@ -0,0 +1,14 @@
+const userInput = ""
+
+function isPalindrome(myWord){
+    if (myWord === "") return undefined;
+    for(let i=0; i < Math.ceil(myWord.toString().length/2); i++){
+        if(myWord.at(i) !== myWord.at(-1-i)){
+            return false
+        }
+    }
+    return true
+}
+
+
+console.log(isPalindrome(userInput))
---
+++ palindroma/readME.md
@@ -0,0 +1,6 @@
+Chiedere all’utente di inserire una parola
+
+Creare una funzione per capire se la parola inserita è palindroma:
+creo un ciclo for da 0 a lungezzaDellaParola/2 arrotondato per eccesso
+
+per ogni i controllo se parola.at(i) e parola at(-1-i) corrispondono
